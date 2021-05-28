5c8c584cc5c79fb7f9eae934e355f51f2f994bfe
Leonard Techel <git@barnslig.eu>
2021-05-28 10:10:09 +0200

    fix(parameters): Fix build after last commit

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import parameters, { tick } from "../features/parameters/parametersSlice";
-
 export const store = configureStore({
-  reducer: {
-    parameters,
-  },
+  reducer: {},
 
   devTools: process.env.NODE_ENV !== "production",
 });
-
-// Update parameters every second
-setInterval(() => store.dispatch(tick()), 1000);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
