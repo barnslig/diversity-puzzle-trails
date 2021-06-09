4cbd6c102f90f2382d014e00d687e062f66a5e0a
Leonard Techel <git@barnslig.eu>
2021-06-09 15:45:40 +0200

    feat: Stay on current page during preloading while navigating

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,9 @@ module.exports = {
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/fileMock.js",
+
+    // See https://github.com/molefrog/wouter/issues/126#issuecomment-667661129
+    "^wouter/(.*)$": "wouter/cjs/$1",
   },
 
   globals: {
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,12 @@
 import { CssBaseline, makeStyles } from "@material-ui/core";
 import { IntlProvider } from "react-intl";
-import { Redirect, Route, Switch } from "wouter";
 import { SnackbarProvider } from "notistack";
 import { ThemeProvider } from "@material-ui/styles";
 import * as React from "react";
 
 import * as messages from "../../lang/de.json";
-import PageLoadingBar from "../common/components/PageLoadingBar";
+import AppRouter from "./AppRouter";
 import theme from "./theme";
-import useGameId from "../common/hooks/useGameId";
-
-const CodePage = React.lazy(() => import("./pages/CodePage"));
-const IndexPage = React.lazy(() => import("./pages/IndexPage"));
-const ScannerPage = React.lazy(() => import("./pages/ScannerPage"));
-const StartPage = React.lazy(() => import("./pages/StartPage"));
 
 const useStyles = makeStyles((theme) => ({
   snackbarContainerRoot: {
@@ -24,7 +17,6 @@ const useStyles = makeStyles((theme) => ({
 
 const App = () => {
   const classes = useStyles();
-  const [gameId] = useGameId();
 
   return (
     <ThemeProvider theme={theme}>
@@ -36,27 +28,7 @@ const App = () => {
           maxSnack={1}
         >
           <CssBaseline />
-
-          <React.Suspense fallback={<PageLoadingBar />}>
-            <Switch>
-              <Route path="/">
-                {gameId ? <IndexPage /> : <Redirect to="/start" />}
-              </Route>
-              <Route path="/scan">
-                {gameId ? <ScannerPage /> : <Redirect to="/start" />}
-              </Route>
-              <Route path="/code/:codeId">
-                {gameId ? (
-                  ({ codeId }) => <CodePage codeId={codeId} />
-                ) : (
-                  <Redirect to="/start" />
-                )}
-              </Route>
-              <Route path="/start/:gameId?">
-                {({ gameId }) => <StartPage gameId={gameId} />}
-              </Route>
-            </Switch>
-          </React.Suspense>
+          <AppRouter />
         </SnackbarProvider>
       </IntlProvider>
     </ThemeProvider>
