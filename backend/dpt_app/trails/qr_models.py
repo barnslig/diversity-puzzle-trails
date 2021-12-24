cbe00fb32f76931e3e05b7390471e525dc9de403
Leonard Techel <git@barnslig.eu>
2021-12-24 23:18:43 +0100

    feat(i18n): Make model labels and field labels translatable

--- backend/dpt_app/trails/qr_models.py
+++ backend/dpt_app/trails/qr_models.py
@@ -1,19 +1,36 @@
+from django.core.files import File
+from django.db import models
+from django.utils.translation import gettext_lazy as _, pgettext_lazy
 import qrcode
 import uuid
+
 from .enums import ParameterType, CharacterType, ActionType
-from django.db import models
-from django.core.files import File
 
 
 class Code(models.Model):
+    class Meta:
+        verbose_name = _('Code')
+        verbose_name_plural = _('Codes')
+
     name = models.CharField(
         max_length=255,
         default="",
-        blank=True
+        blank=True,
+        verbose_name=pgettext_lazy("Code Name", "Name")
     )
-    one_shot = models.BooleanField(default=False)
-    uuid = models.UUIDField(default=uuid.uuid4, unique=True)
-    image = models.ImageField(blank=True)
+    one_shot = models.BooleanField(
+        default=False,
+        verbose_name=_("One-Shot")
+    )
+    uuid = models.UUIDField(
+        default=uuid.uuid4,
+        unique=True,
+        verbose_name=_("UUID")
+    )
+    image = models.ImageField(
+        blank=True,
+        verbose_name=_("Image")
+    )
 
     def __str__(self):
         ret = ""
@@ -35,26 +52,37 @@ class Code(models.Model):
 
 
 class Action(models.Model):
+    class Meta:
+        verbose_name = _('Action')
+        verbose_name_plural = _('Actions')
+
     code = models.ForeignKey(
         'Code',
         on_delete=models.CASCADE,
-        related_name="actions"
+        related_name="actions",
+        verbose_name=_("Code")
     )
     action_type = models.CharField(
         max_length=2,
         choices=ActionType.choices,
         default=ActionType.NONE,
+        verbose_name=_("Action Type")
     )
     parameter = models.CharField(
         max_length=2,
         choices=ParameterType.choices,
         default=ParameterType.NONE,
+        verbose_name=_("Parameter")
     )
-    value = models.IntegerField(default=0)
+    value = models.IntegerField(
+        default=0,
+        verbose_name=_("Value")
+    )
     character = models.CharField(
         max_length=2,
         choices=CharacterType.choices,
         default=CharacterType.NONE,
+        verbose_name=_("Character")
     )
 
     def label(self):
