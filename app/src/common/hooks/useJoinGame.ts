2a89eb9bcc79bf58cf9f05e6107ac0d525fbc736
Leonard Techel <git@barnslig.eu>
2021-12-30 15:35:53 +0100

    feat: Check for game existence before joining it

--- app/src/common/hooks/useJoinGame.ts
+++ app/src/common/hooks/useJoinGame.ts
@@ -1,9 +1,12 @@
 import { useIntl } from "react-intl";
 import { useLocation } from "wouter";
 import { useSnackbar } from "notistack";
+import * as React from "react";
 
-import useGameId from "../../common/hooks/useGameId";
+import config from "../../config";
+import errorAwareFetcher from "./api/helper/errorAwareFetcher";
 import useCharacter from "./useCharacter";
+import useGameId from "../../common/hooks/useGameId";
 
 /**
  * A React hook to join a game using a game ID
@@ -11,24 +14,47 @@ import useCharacter from "./useCharacter";
  * @returns A function to be called with a game ID
  */
 const useJoinGame = () => {
-  const [, setGameId] = useGameId();
+  const [, setGameId, deleteGameId] = useGameId();
   const [, setLocation] = useLocation();
   const [, , deleteCharacter] = useCharacter();
   const { enqueueSnackbar } = useSnackbar();
   const intl = useIntl();
 
-  return (gameId: string) => {
-    deleteCharacter();
-    setGameId(gameId);
-    enqueueSnackbar(
-      intl.formatMessage({
-        defaultMessage: "Du bist jetzt Teil vom Spiel!",
-        description: "success snackbar on join game",
-      }),
-      { variant: "success" }
-    );
-    setLocation("/");
-  };
+  return React.useCallback(
+    async (gameId: string) => {
+      try {
+        // Retrieve the game manifest
+        const url = config.apiEndpoints.game(gameId);
+        await errorAwareFetcher(() => fetch(url));
+
+        deleteCharacter();
+        setGameId(gameId);
+
+        enqueueSnackbar(
+          intl.formatMessage({
+            defaultMessage: "Du bist jetzt Teil vom Spiel!",
+            description: "success snackbar on join game",
+          }),
+          { variant: "success" }
+        );
+
+        setLocation("/");
+      } catch (e) {
+        deleteCharacter();
+        deleteGameId();
+
+        throw e;
+      }
+    },
+    [
+      deleteCharacter,
+      deleteGameId,
+      enqueueSnackbar,
+      intl,
+      setGameId,
+      setLocation,
+    ]
+  );
 };
 
 export default useJoinGame;
