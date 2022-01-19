a51be4d4a40bf64f5d9f46b352a1ebf745860755
Leonard Techel <git@barnslig.eu>
2022-01-19 18:43:23 +0100

    feat(clock): Implement new, easier to handle, more efficient clock algorithms
    + Add fixup buttons that add minutes or points to a game that is game
    overed

--- backend/dpt_app/trails/models.py
+++ backend/dpt_app/trails/models.py
@@ -1,4 +1,9 @@
+from django.contrib import admin
 from django.db import models
+from django.db.models import F, Min
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _, pgettext_lazy
 
 from .enums import ClockType, ParameterType, ParameterScope, CharacterType
@@ -25,13 +30,22 @@ class Game(models.Model):
         verbose_name=_("Clock State")
     )
 
-    # 1000 => Default speed, "real time"
-    # 2000 => Half-Speed, "slow time"
-    #  500 => Double speed, "fast time"
-    # amount of miliseconds resulting in one 'tick'
-    clock_speed = models.IntegerField(
-        default=1000,
+    # 0.5 => Half-Speed, "slow time"
+    # 1.0 => Default speed, "real time"
+    # 2.0 => Double speed, "fast time"
+    clock_speed = models.FloatField(
+        default=1,
         verbose_name=_("Clock Speed")
+    )
+
+    clock_last_change = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_("Last clock state change")
+    )
+
+    clock_duration = models.IntegerField(
+        default=0,
+        verbose_name=_("Total clock running duration")
     )
 
     hasUserParameterScope = models.BooleanField(
@@ -44,8 +58,82 @@ class Game(models.Model):
         verbose_name=_("Has Messages Feature?")
     )
 
+    @property
+    @admin.display(description=_("Maximum Game Duration"))
+    def max_clock_duration(self):
+        """Get the maximum clock duration"""
+        return Parameter.max_clock_duration(self)
+
+    @property
+    @admin.display(description=_("Total Game Duration"))
+    def total_clock_duration(self):
+        """Get the current clock duration"""
+        clock_duration = self.clock_duration
+        if self.clock_state == ClockType.RUNNING:
+            clock_duration = self.clock_duration + \
+                timezone.now().timestamp() - self.clock_last_change.timestamp()
+
+        return min(self.max_clock_duration, clock_duration)
+
+    @property
+    @admin.display(description=_("Is Game Over?"))
+    def is_game_over(self):
+        """Get whether the game is in game over state"""
+        return self.total_clock_duration == self.max_clock_duration
+
+    def resolve_game_over_add_points(self, num_points):
+        """Resolve a game over state by adding points to the parameter which is causing the game over"""
+        if not self.is_game_over:
+            return
+
+        for param in self.parameter.all():
+            if param.current_value == 0:
+                self.clock_last_change = timezone.now()
+                self.save()
+
+                param.value += num_points
+                param.save()
+
+                return param
+
+    def resolve_game_over_add_time(self, num_time):
+        """Resolve a game over state by adding time to the game"""
+        self.clock_state = ClockType.RUNNING
+        self.clock_last_change = timezone.now()
+        self.clock_duration = max(0, self.clock_duration - num_time)
+        self.save()
+
+    def reset(self):
+        """Reset a game by resetting the clock and added parameter values"""
+        self.clock_state = ClockType.STOPPED
+        self.clock_duration = 0
+        self.clock_last_change = timezone.now()
+        self.save()
+
+        self.parameter.all().update(value=0)
+
     def __str__(self):
         return self.name
+
+
+@receiver(pre_save, sender=Game)
+def update_clock_last_change(sender, instance: Game, *args, **kwargs):
+    # Do nothing when no id is set. This is the case when a new model instance gets created
+    if not instance.id:
+        return
+
+    clock_current_state = Game.objects.get(id=instance.id).clock_state
+    clock_next_state = instance.clock_state
+
+    if clock_current_state != clock_next_state:
+        if clock_current_state == ClockType.RUNNING:
+            next_clock_duration = instance.clock_duration + \
+                timezone.now().timestamp() - instance.clock_last_change.timestamp()
+
+            instance.clock_duration = min(
+                instance.max_clock_duration, next_clock_duration)
+
+        instance.clock_last_change = timezone.now()
 
 
 class Parameter(models.Model):
@@ -67,17 +155,30 @@ class Parameter(models.Model):
         verbose_name=_("Scope")
     )
 
-    min_value = 0
+    min_value = models.IntegerField(
+        default=0,
+        verbose_name=_("Minimum Value")
+    )
 
     # From the documentation
     # "Values from -2147483648 to 2147483647 are safe in all databases supported by Django."
-    max_value = 2147483647
+    max_value = models.IntegerField(
+        default=2147483647,
+        verbose_name=_("Maximum Value")
+    )
+
+    initial_value = models.IntegerField(
+        verbose_name=_("Initial Value")
+    )
+
     value = models.IntegerField(
+        default=0,
         verbose_name=_("Value")
     )
 
     # Amount to subtrac during one "tick"
-    rate = models.IntegerField(
+    rate = models.FloatField(
+        default=0,
         verbose_name=_("Rate")
     )
     game = models.ForeignKey(
@@ -86,6 +187,33 @@ class Parameter(models.Model):
         related_name="parameter",
         verbose_name=_("Game")
     )
+
+    @staticmethod
+    def max_clock_duration(game: Game):
+        """Get the maximum game clock duration at which the first parameter is zero"""
+        query = Parameter.objects.filter(game=game).annotate(
+            dur_when_zero=-1 * (F('initial_value') + F('value')) / (F('game__clock_speed') * F('rate')))
+
+        return query.aggregate(Min('dur_when_zero'))['dur_when_zero__min']
+
+    def value_at(self, clock_duration):
+        """Get the parameter value at a specific game clock duration"""
+        value = round((self.initial_value + self.value) + clock_duration
+                      * self.game.clock_speed * self.rate)
+
+        return max(min(value, self.max_value), self.min_value)
+
+    @property
+    @admin.display(description=_("Current Value"))
+    def current_value(self):
+        """Get the current parameter value, with respect to the current game clock duration"""
+        return self.value_at(self.game.total_clock_duration)
+
+    @property
+    @admin.display(description=_("Clock Duration When Zero"))
+    def game_clock_duration_when_value_zero(self):
+        """Get the game.clock_duration at which this parameter is zero"""
+        return -1 * (self.initial_value + self.value) / (self.game.clock_speed * self.rate)
 
     def label(self):
         return ParameterType(self.name).label
--- backend/dpt_app/trails/tests.py
+++ backend/dpt_app/trails/tests.py
@@ -1,3 +1,186 @@
+from datetime import datetime
 from django.test import TestCase
+from django.utils import timezone
+from unittest.mock import patch
 
-# Create your tests here.
+from .enums import CharacterType, ClockType, ParameterScope, ParameterType
+from .models import Character, Game, Parameter, Player
+
+CLOCK_DURATION = 70
+MAX_CLOCK_DURATION = 80
+
+
+class GameTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.game: Game = Game.objects.create(
+            name="Test Game",
+            slug="test-game",
+            clock_duration=CLOCK_DURATION
+        )
+
+        # Manually set clock_last_change as it otherwise gets overridden by auto_now_add=True
+        cls.game.clock_last_change = timezone.make_aware(
+            datetime(2022, 1, 15, 12, 00))
+        cls.game.save()
+
+        cls.param: Parameter = Parameter.objects.create(
+            name=ParameterType.ENERGY,
+            scope=ParameterScope.GLOBAL,
+            initial_value=100,
+            min_value=0,
+            max_value=500,
+            rate=-1,
+            game=cls.game
+        )
+
+        cls.param2: Parameter = Parameter.objects.create(
+            name=ParameterType.FOOD,
+            scope=ParameterScope.GLOBAL,
+            initial_value=80,
+            rate=-0.8,
+            game=cls.game
+        )
+
+        cls.param3: Parameter = Parameter.objects.create(
+            name=ParameterType.HYGIENE,
+            scope=ParameterScope.GLOBAL,
+            initial_value=120,
+            rate=-1.5,
+            game=cls.game
+        )
+
+        cls.character = Character.objects.create(
+            character_class=CharacterType.ENGINEER
+        )
+
+        cls.player = Player.objects.create(
+            name="Test Player",
+            bearer="Bearer test123",
+            game=cls.game,
+            character=cls.character
+        )
+
+    def cause_game_over(self):
+        self.game.clock_state = ClockType.RUNNING
+        self.game.clock_duration = MAX_CLOCK_DURATION
+        self.game.save()
+
+
+class GameModelTest(GameTestCase):
+    def test_max_clock_duration(self):
+        # it returns the maximum clock duration
+        self.assertEqual(self.game.max_clock_duration, MAX_CLOCK_DURATION)
+
+    def test_total_clock_duration(self):
+        # it updates the total_clock_duration at maximum to the max_clock_duration
+        self.assertEqual(self.game.clock_state, ClockType.STOPPED)
+        self.assertEqual(self.game.total_clock_duration, CLOCK_DURATION)
+
+        self.game.clock_duration = 9999
+        self.assertEqual(self.game.total_clock_duration, MAX_CLOCK_DURATION)
+
+        # it updates the total_clock_duration according to the current time when running
+        with patch('django.utils.timezone.now') as mock_timezone_now:
+            mock_timezone_now.return_value = datetime(2022, 1, 15, 12, 10)
+
+            self.game.clock_state = ClockType.RUNNING
+            self.assertEqual(self.game.total_clock_duration,
+                             CLOCK_DURATION + 10)
+
+            mock_timezone_now.return_value = datetime(2100, 1, 1, 0, 0)
+            self.assertEqual(self.game.total_clock_duration,
+                             MAX_CLOCK_DURATION)
+
+    def test_is_game_over(self):
+        # cause a game over
+        self.cause_game_over()
+
+        # it is in game over state
+        self.assertEqual(self.game.is_game_over, True)
+
+    def test_resolve_game_over_add_points(self):
+        # it does nothing when the game is not in game over state
+        self.assertEqual(self.game.is_game_over, False)
+        self.assertEqual(self.game.resolve_game_over_add_points(10), None)
+
+        # cause a game over
+        self.cause_game_over()
+
+        # it adds points and resolves the game over
+        param = self.game.resolve_game_over_add_points(10)
+        self.assertEqual(self.game.is_game_over, False)
+        self.assertEqual(param, self.param3)
+        self.assertEqual(param.current_value, 10)
+
+    def test_resolve_game_over_add_time(self):
+        # it adds time when the game is not in game over state
+        self.assertEqual(self.game.is_game_over, False)
+
+        current_total_duration = self.game.total_clock_duration
+        self.game.resolve_game_over_add_time(10)
+
+        self.assertEqual(self.game.is_game_over, False)
+        self.assertEqual(self.game.clock_state, ClockType.RUNNING)
+        self.assertAlmostEqual(
+            self.game.total_clock_duration,
+            current_total_duration - 10,
+            0
+        )
+
+        # cause a game over
+        self.cause_game_over()
+
+        # it resolves a game over
+        self.assertEqual(self.game.is_game_over, True)
+
+        current_total_duration = self.game.total_clock_duration
+        self.game.resolve_game_over_add_time(10)
+
+        self.assertEqual(self.game.is_game_over, False)
+        self.assertEqual(self.game.clock_state, ClockType.RUNNING)
+        self.assertAlmostEqual(
+            self.game.total_clock_duration,
+            current_total_duration - 10,
+            0
+        )
+
+        # it does not turn the game duration into negative time
+        self.game.resolve_game_over_add_time(99999)
+        self.assertAlmostEqual(self.game.total_clock_duration, 0, 0)
+
+    def test_reset(self):
+        self.game.reset()
+
+        # it resets the game clock duration to zero and stops the game
+        self.assertEqual(self.game.clock_state, ClockType.STOPPED)
+        self.assertEqual(self.game.total_clock_duration, 0)
+
+    def test_update_clock_last_change(self):
+        # it updates only the clock_last_change when switching the clock_state from stopped to running
+        with patch('django.utils.timezone.now') as mock_timezone_now:
+            self.assertEqual(self.game.clock_state, ClockType.STOPPED)
+
+            now = timezone.make_aware(datetime(2022, 1, 15, 12, 10))
+            mock_timezone_now.return_value = now
+
+            self.game.clock_state = ClockType.RUNNING
+            self.game.save()
+
+            self.assertEqual(self.game.clock_state, ClockType.RUNNING)
+            self.assertEqual(self.game.clock_last_change, now)
+            self.assertEqual(self.game.clock_duration, CLOCK_DURATION)
+
+        # it updates clock_last_change and clock_duration when switching the clock_state from running to stopped
+        with patch('django.utils.timezone.now') as mock_timezone_now:
+            self.assertEqual(self.game.clock_state, ClockType.RUNNING)
+
+            now = timezone.make_aware(datetime(2022, 1, 15, 12, 20))
+            mock_timezone_now.return_value = now
+
+            self.game.clock_state = ClockType.STOPPED
+            self.game.save()
+
+            self.assertEqual(self.game.clock_state, ClockType.STOPPED)
+            self.assertEqual(self.game.clock_last_change, now)
+            self.assertEqual(self.game.clock_duration, CLOCK_DURATION + 10)
