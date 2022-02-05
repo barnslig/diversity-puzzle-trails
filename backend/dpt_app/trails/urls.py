2972066863cb13526f9b1758a6aa36e8f68e9ede
Leonard Techel <git@barnslig.eu>
2022-02-05 19:53:53 +0100

    feat(backend): Switch API to django-ninja, implement Player API
    This commit implements two major changes and fixes one bug:
    
    - It switches to django-ninja for the API implementation which makes it
      easier to parse request bodies
    - It implements a new Player API that currently can only be used to
      create a new player using HTTP PUT. The Player API replaces creating a
      player via character QR code, thus allowing API calls to the game
      before choosing a character, resulting in less error messages in the
      app.
    - Using django-ninja, the commit fixes the HTTP POST action of the Clock
      API, allowing users to pause the game.

--- backend/dpt_app/trails/urls.py
+++ backend/dpt_app/trails/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
 
-from . import views
+from .api import api
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('games/<str:gameId>', views.gameManifest, name='gameManifest'),
-    path('games/<str:gameId>/clock', views.clock, name='clock'),
-    path('games/<str:gameId>/parameters', views.parameter, name='parameter'),
-    path('games/<str:gameId>/messages', views.messages, name='message'),
-    path('games/<str:gameId>/codes/<str:codeId>', views.code, name='code'),
+    path("", api.urls),
 ]
