b824179ab0ac58b7af13370a63549c8813c0358a
Leonard Techel <git@barnslig.eu>
2022-02-10 19:50:21 +0100

    refactor(i18n): Switch to manual messages compilation. Add merge script
    This commit replaces complicated message extraction and compilation
    using two manual scripts in package.json. Furthermore, it replaces
    complicated jq tasks with a new, formatjs-tailored extraction script.

--- app/src/app/App.tsx
+++ app/src/app/App.tsx
@@ -3,7 +3,7 @@ import { IntlProvider } from "react-intl";
 import { StyledEngineProvider, ThemeProvider } from "@mui/material";
 import * as React from "react";
 
-import * as messages from "../../lang/de.json";
+import * as messages from "../../lang-compiled/de.json";
 import theme from "./theme";
 
 import AppRouter from "./AppRouter";
@@ -15,7 +15,6 @@ const App = () => {
   return (
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={theme}>
-        {/* @ts-expect-error */}
         <IntlProvider messages={messages} locale="de" defaultLocale="de">
           <NotificationProvider>
             <SWRProvider>
