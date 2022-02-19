02d0fb4074eb6a7e16fd963dc3422e05c9b0d1c5
Leonard Techel <git@barnslig.eu>
2022-02-19 15:43:53 +0100

    fix: Only show one action skeleton on the code page

--- app/src/features/code/Code.tsx
+++ app/src/features/code/Code.tsx
@@ -12,32 +12,27 @@ type CodeProps = {
 const Code = ({ code }: CodeProps) => {
   return (
     <List>
-      {!code
-        ? Array(3)
-            .fill(0)
-            .map((_, i) => (
-              <ListItem key={i}>
-                <ListItemIcon>
-                  <Skeleton variant="circular" width={24} height={24} />
-                </ListItemIcon>
-                <ListItemText
-                  primary={<Skeleton width={24} height={24} />}
-                  secondary={<Skeleton width={140} height={20} />}
-                />
-              </ListItem>
-            ))
-        : code.attributes.actions.map((action, i) => {
-            const { icon, value, label } = codeActionMap[action.type];
-            return (
-              <ListItem key={`${action.type}-${i}`}>
-                <ListItemIcon>{icon}</ListItemIcon>
-                <ListItemText
-                  primary={value(action)}
-                  secondary={label(action)}
-                />
-              </ListItem>
-            );
-          })}
+      {!code ? (
+        <ListItem>
+          <ListItemIcon>
+            <Skeleton variant="circular" width={24} height={24} />
+          </ListItemIcon>
+          <ListItemText
+            primary={<Skeleton width={24} height={24} />}
+            secondary={<Skeleton width={140} height={20} />}
+          />
+        </ListItem>
+      ) : (
+        code.attributes.actions.map((action, i) => {
+          const { icon, value, label } = codeActionMap[action.type];
+          return (
+            <ListItem key={`${action.type}-${i}`}>
+              <ListItemIcon>{icon}</ListItemIcon>
+              <ListItemText primary={value(action)} secondary={label(action)} />
+            </ListItem>
+          );
+        })
+      )}
     </List>
   );
 };
